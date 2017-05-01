558d291b3c10fca3035b0519d9e32e7595fb9dda
fbrahman <fbrahman@gmail.com>
2017-04-30 21:54:46 -0700

    wireframe done. Character images found.

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -1,3 +1,3 @@
-Game Object 
-will have another object called chracters
-Game object will have different functions for battling
+// Game Object 
+// will have another object called chracters
+// Game object will have different functions for battling
