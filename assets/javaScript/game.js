8e53d8bbd10db377b759d6ba17ba41de56f331a0
fbrahman <fbrahman@gmail.com>
2017-05-04 16:09:59 -0700

    moved all character functions into the game object

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -7,26 +7,69 @@ var game = {
 	characterArray:[],
 
 	playerChoice: function(x){
-	
-		// if (playerCharacter ===""){}
+		if (this.playerCharacter ===""){
+			var indexValue = $(x).find("img.warriorImg").attr("indexValue");
+			this.playerCharacter = this.characterArray[indexValue];
 			$(x).find("img.warriorImg").detach().appendTo("#playerChoiceImage");
-			
+		}
+		else{
+			this.computerChoice(x);
+		}	
 	},	
 
-	computerChoice: function(){
-
+	computerChoice: function(x){
+		if (this.computerCharacter ===""){
+			var indexValue = $(x).find("img.warriorImg").attr("indexValue");
+			this.computerCharacter = this.characterArray[indexValue];
+			$(x).find("img.warriorImg").detach().appendTo("#computerChoiceImage");
+		}
+		else{
+			return;
+		}
+	//show attack button after computer is selected.
 	},
 
 	playerAttack: function (){
-
+		this.playerCharacter.cAttackCounter++;
+		this.damage(this.playerCharacter);
+		this.powerLevelIncrease(this.playerCharacter);
 	},
 
 	computerAttack: function(){
-
+		this.damage(this.computerCharacter);
 	},
 
-	damage: function (){
+	damage: function (x){
+		console.log("this is damage",x.cAttack);
+		//display current attack level  as damage for opposing character
+		this.hpUpdate(x);
+	},
 
+	hpUpdate: function(x){
+		if (x === this.playerCharacter){
+			console.log("the player called me")
+		}
+		else if (x === this.computerCharacter){
+			console.log("the computer called me")
+		}
+		else{
+			console.log("you should not be seeing this")
+		}
+	},
+
+	powerLevelIncrease: function (x) {
+		x.cPL = x.cPL+1000;
+		console.log("this is new PL",x.cPL);
+		this.attackLevelIncrease(x);
+		//write new PL on screen
+	},
+
+	attackLevelIncrease: function (x){
+		console.log("this is attack before change",x.cAttack);
+		console.log("this is x.CPL/100", x.cPL/100);
+		x.cAttack = x.cAttack+(x.cPL/500);
+		console.log("this is new attack",x.cAttack);
+		//write new attack on screen
 	},
 
 	winLoss: function (){
@@ -39,21 +82,30 @@ var game = {
 
 	setGameBoard: function(){
 		for(i = 0; i < this.characterArray.length; i++){
-			var charImg = $("<img>");
+			// var charImg = $("<img>");
+			// charImg.addClass("warriorPic");
+			// charImg.attr("src",this.characterArray[i].cImg);
+			// charImg.attr("indexValue", i);
+			// $("#warriorFlex").append(charImg);
+			// console.log(this.characterArray[i].cImg);
+			var $elem = $("#warriorFlex");
 
-			charImg.addClass("warriorPic");
-
-			charImg.attr("src",this.characterArray[i].cImg);
-
-			charImg.attr("indexValue", i);
-
-			$("#warriorFlex").append(charImg);
-
-			console.log(this.characterArray[i].cImg);
+			$elem.append(
+					$("<div>",{"class":"warriorSelection"}).append(
+						$("<div>", {"class":"warriorPic"}).append(
+							$("<img>",{"class":"warriorImg","src":this.characterArray[i].cImg, "indexValue": i})
+						)
+					).append(
+					$("<div>", {"class":"warriorName"}).append(
+						$("<p>",{"class":"name", text:this.characterArray[i].cName})
+						)
+					)
+				);	
 		}
 	},
 
 	reset: function (){
+		this.setGameBoard();
 		$(".warriorSelection").click(function(){
 		game.playerChoice(this);
 	// console.log(this);
@@ -73,25 +125,17 @@ function characters(characterName, characterHP, characterAttack, characterPowerL
 	game.characterArray.push(this);
 };
 
-characters.protoype = {
-	constructor:characters,
+// characters.protoype = {
+// 	constructor:characters,
 
-	powerLevelIncrease: function () {
-		this.characterPowerLevel = this.cAttackCounter*this.characterPowerLevel
-	},
+// 	powerLevelIncrease: function () {
+// 		this.characterPowerLevel = this.cAttackCounter*this.characterPowerLevel
+// 	},
 
-	attackLevelIncrease: function (){
-		this.cAttack = this.cAttack*(this.characterPowerLevel/1000)
-	},
-
-	hpUpdate: function(){
-
-	},
-
-	attackCounterUpdate: function (){
-
-	}
-};
+// 	attackLevelIncrease: function (){
+// 		this.cAttack = this.cAttack*(this.characterPowerLevel/1000)
+// 	},
+// };
 
 var character1 = new characters("Goku", 120, 10, 1000);
 var character2 = new characters("Krillin", 100, 8, 800);
